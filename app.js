a28ac8f1d06a920d4930287d67dac91e26027c12
Yatin <yatinrathod612@gmail.com>
2022-07-07 23:43:14 +0530

    Added PORT

--- app.js
+++ app.js
@@ -84,6 +84,8 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-app.listen(3000, function() {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
